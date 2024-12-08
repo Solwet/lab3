2478ba241e671172e41989d4e3054873ebae608f
David44ru <Peresypkin.dovid@gmail.com>
2024-12-08 22:47:38 +0500

    сделали атрибуты приватными и добавили геттеры

--- ElementPeresypkin/ElementPeresypkin.py
+++ ElementPeresypkin/ElementPeresypkin.py
@@ -1,10 +1,30 @@
 class ElementPeresypkin:
     def __init__(self, name, symbol, number):
-        # Инициализация атрибутов
-        self.name = name
-        self.symbol = symbol
-        self.number = number
+        # Приватные атрибуты
+        self.__name = name
+        self.__symbol = symbol
+        self.__number = number
 
     def dump(self):
         # Вывод информации об элементе
-        print(f"Название: {self.name}, Символ: {self.symbol}, Порядковый номер: {self.number}")
+        print(f"Название: {self.__name}, Символ: {self.__symbol}, Порядковый номер: {self.__number}")
+
+    # Геттеры
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def symbol(self):
+        return self.__symbol
+
+    @property
+    def number(self):
+        return self.__number
+
+
+# Создаем объект для элемента "Сера"
+element = ElementPeresypkin("Сера", "S", 16)
+
+# Выводим данные об элементе
+element.dump()
