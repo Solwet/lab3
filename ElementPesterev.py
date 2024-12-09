f59366985d9d88cfe26cd628971cca8fe93c708a
AndreyPesterev <pesterev.pae@yandex.ru>
2024-12-09 11:59:45 +0500

    Добавил в ElementPesterev метод dump()

--- ElementPesterev.py
+++ ElementPesterev.py
@@ -1,12 +1,13 @@
-class ElementPesterev:
-    def __init__(self, name, symbol, number):
-        self.name = name
-        self.symbol = symbol
-        self.number = number
+def __repr__(self):
+    return f"Element: {self.name} (Symbol: {self.symbol}, Atomic Number: {self.number})"
 
-    def __repr__(self):
-        return f"Element: {self.name} (Symbol: {self.symbol}, Atomic Number: {self.number})"
+def dump(self):
+    # Метод для вывода значений атрибутов объекта
+    print(f"Element Name: {self.name}")
+    print(f"Symbol: {self.symbol}")
+    print(f"Atomic Number: {self.number}")
 
 chlor = ElementPesterev(name="Chlor", symbol="Cl", number=17)
 
 print(chlor)
+chlor.dump()
